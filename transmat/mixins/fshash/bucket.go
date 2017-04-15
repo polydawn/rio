bdfa6740417a1a716e82a15ae422c6dee351276e
Eric Myhre <hash@exultant.us>
2017-04-15 02:45:25 +0200

    Port over the fshash bucket stuff.
    Treewalk lib comes too, no patching required.
    
    There's a few things that feel a little crotchety in this fshash bucket implementation, but overall the codesmells from this have been fairly low over time, so we'll keep it; some smoothing later is possible, but shouldn't be a collosal breakage thing.
    
    The filename strings are getting duplicated now as of the port, to compensate for the fact we're mostly operating in terms of RelPath objects, and our hashing and sorting and traversal standards believe pretty fiercely in the trailing-slash-for-dirs convention.  This seems not ideal for performance, but also... yeah, we'll survive.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ transmat/mixins/fshash/bucket.go
@@ -0,0 +1,82 @@
+package fshash
+
+import (
+	"os"
+
+	"go.polydawn.net/rio"
+	"go.polydawn.net/rio/fs"
+	"go.polydawn.net/rio/lib/treewalk"
+)
+
+/*
+	Bucket keeps hashes of file content and the set of metadata per file and dir.
+	This is to make it possible to range over the filesystem out of order and
+	construct a total hash of the system in order later.
+*/
+type Bucket interface {
+	AddRecord(metadata fs.Metadata, contentHash []byte) // record a file into the bucket
+	Iterator() (rootRecord RecordIterator)              // return a treewalk root that does a traversal ordered by path
+	Root() Record                                       // return the 0'th record; is the root if `Iterator` has already been invoked.
+	Length() int
+}
+
+type Record struct {
+	// Name field is *almost* identical to Metadata.Name.String(), but includes trailing slashes for dirs.
+	Name        string
+	Metadata    fs.Metadata
+	ContentHash []byte
+}
+
+/*
+	Error raised while the bucket is being iterated and we discover it has either
+	a duplicate entry or a missing parent dir for some entry.
+
+	If you're getting this after walking a filesystem, you had a bug in your walk;
+	if you're getting it after walking a tar or some format, it may be missing entries as well.
+
+	This is not meant to be user facing; map it to something meaningful in your caller.
+*/
+type ErrInvalidFilesystem struct {
+	Msg string
+}
+
+// for sorting
+type recordsByFilename []Record
+
+func (a recordsByFilename) Len() int           { return len(a) }
+func (a recordsByFilename) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a recordsByFilename) Less(i, j int) bool { return a[i].Name < a[j].Name }
+
+/*
+	RecordIterator is used for walking Bucket contents in hash-ready order.
+
+	May panic with ErrInvalidFilesystem during traversal.
+	TODO : reconsider if maybe you wouldn't like a "sancheck" method that does that scan first?
+*/
+type RecordIterator interface {
+	treewalk.Node
+	Record() Record
+}
+
+/*
+	Node used for the root (Name = "./") path, if one isn't provided.
+*/
+var DefaultRoot Record
+
+func init() {
+	DefaultRoot = DefaultDirRecord()
+}
+
+/*
+	Returns a new, consistent, "blank" record for a directory.
+	You must assign the `Name` metadata.
+*/
+func DefaultDirRecord() Record {
+	return Record{
+		Metadata: fs.Metadata{
+			Mode:  0755 + os.ModeDir,
+			Mtime: rio.FilterDefaultMtime,
+		},
+		ContentHash: nil,
+	}
+}
---
+++ transmat/mixins/fshash/bucket_memory.go
@@ -0,0 +1,98 @@
+package fshash
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"go.polydawn.net/rio/fs"
+	"go.polydawn.net/rio/lib/treewalk"
+)
+
+var _ Bucket = &MemoryBucket{}
+
+type MemoryBucket struct {
+	// my kingdom for a red-black tree or other sane sorted map implementation
+	lines []Record
+}
+
+func (b *MemoryBucket) AddRecord(metadata fs.Metadata, contentHash []byte) {
+	name := metadata.Name.String()
+	if metadata.Mode.IsDir() {
+		name += "/"
+	}
+	b.lines = append(b.lines, Record{name, metadata, contentHash})
+}
+
+/*
+	Get a `treewalk.Node` that starts at the root of the bucket.
+	The walk will be in deterministic, sorted order (and thus is appropriate
+	for hashing).
+
+	This applies some "finalization" operations before starting the walk:
+	  - All records will be sorted.
+	  - If there isn't a root node (i.e. Name = "./"), one will be added.
+
+	This is only safe for non-concurrent use and depth-first traversal.
+	If the data structure is changed, or (sub)iterators used out of order,
+	behavior is undefined.
+*/
+func (b *MemoryBucket) Iterator() RecordIterator {
+	sort.Sort(recordsByFilename(b.lines))
+	// check for rootedness.  make a reasonable default one if not present.
+	if len(b.lines) == 0 || b.lines[0].Metadata.Name != (fs.RelPath{}) {
+		// this is stupidly expensive but checking for it without sorting would be even worse, so
+		lines := make([]Record, len(b.lines)+1)
+		lines[0] = DefaultRoot
+		copy(lines[1:], b.lines)
+		b.lines = lines
+	}
+	var that int
+	return &memoryBucketIterator{b.lines, 0, &that}
+}
+
+func (b *MemoryBucket) Root() Record {
+	return b.lines[0]
+}
+
+func (b *MemoryBucket) Length() int {
+	return len(b.lines)
+}
+
+type memoryBucketIterator struct {
+	lines []Record
+	this  int  // pretending a linear structure is a tree is weird.
+	that  *int // this is the last child walked.
+}
+
+func (i *memoryBucketIterator) NextChild() treewalk.Node {
+	// Since we sorted before starting iteration, all child nodes are contiguous and follow their parent.
+	// Each treewalk node keeps its own record's index (implicitly, this is forming a stack),
+	// and they all share the same value for last index walked, so when a child has been fully iterated over,
+	// the next call on the parent will start looking right after all the child's children.
+	next := *i.that + 1
+	if next >= len(i.lines) {
+		return nil
+	}
+	nextName := i.lines[next].Name
+	thisName := i.lines[i.this].Name
+	// is the next one still a child?
+	if strings.HasPrefix(nextName, thisName) {
+		// check for missing trees
+		if strings.ContainsRune(nextName[len(thisName):len(nextName)-1], '/') {
+			panic(ErrInvalidFilesystem{fmt.Sprintf("missing tree: %q followed %q", nextName, thisName)})
+		}
+		// check for repeated names
+		if i.lines[*i.that].Name == nextName {
+			panic(ErrInvalidFilesystem{fmt.Sprintf("repeated path: %q", nextName)})
+		}
+		// step forward
+		*i.that = next
+		return &memoryBucketIterator{i.lines, *i.that, i.that}
+	}
+	return nil
+}
+
+func (i memoryBucketIterator) Record() Record {
+	return i.lines[i.this]
+}
