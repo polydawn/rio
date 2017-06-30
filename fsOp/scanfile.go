6eaa8ef2147537736e2f0c666012b0b039e3585c
Eric Myhre <hash@exultant.us>
2017-06-29 18:49:52 -0700

    Port scanfile function.
    No more `import "archive/tar"`!  Yay, victory heuristic passed.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ fsOp/scanfile.go
@@ -0,0 +1,32 @@
+package fsOp
+
+import (
+	"io"
+	"os"
+
+	"go.polydawn.net/rio/fs"
+)
+
+/*
+	Scan file attributes into an `fs.Metadata` struct, and return an
+	`io.ReadCloser` for the file content.
+
+	The reader is nil if the path is any type other than a file.  If a
+	reader is returned, the caller is expected to close it.
+*/
+func ScanFile(afs fs.FS, path fs.RelPath) (fmeta *fs.Metadata, body io.ReadCloser, err fs.ErrFS) {
+	// most of the heavy work is already done by fs.Lstat; this method just adds the file content.
+	fmeta, err = afs.LStat(path)
+	if err != nil {
+		return fmeta, nil, err
+	}
+	switch fmeta.Type {
+	case fs.Type_File:
+		var err error
+		body, err = afs.OpenFile(path, os.O_RDONLY, 0)
+		if err != nil {
+			return fmeta, body, fs.IOError(err)
+		}
+	}
+	return
+}
