33ee2c57c98d8820ee391f8106f70e76dcbcfd47
Eric Myhre <hash@exultant.us>
2017-08-08 13:33:05 -0500

    Begin to backfill tests for placefile.
    Fix one fairly critical infinite loop issue.  Testing, kids.  Don't merge branches without it.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- fsOp/placefile_test.go
+++ fsOp/placefile_test.go
@@ -1,11 +1,13 @@
 package fsOp
 
 import (
+	"bytes"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 
 	"go.polydawn.net/rio/fs"
+	"go.polydawn.net/rio/fs/osfs"
 	"go.polydawn.net/rio/testutil"
 )
 
@@ -13,6 +15,12 @@ func TestPlaceFile(t *testing.T) {
 	Convey("PlaceFile suite:", t, func() {
 		testutil.WithTmpdir(func(tmpDir fs.AbsolutePath) {
 			Convey("Simple file placements", func() {
+				afs := osfs.New(tmpDir)
+				err := PlaceFile(afs, fs.Metadata{
+					Name: fs.MustRelPath("thing"),
+					Type: fs.Type_File,
+				}, bytes.NewBuffer([]byte("abc\n")), true)
+				So(err, ShouldBeNil)
 				// TODO
 			})
 			Convey("Simple dir placements", func() {
