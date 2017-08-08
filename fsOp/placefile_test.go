40f4f230e52c4a333523cca5fc85f80ca0b1a4c5
Eric Myhre <hash@exultant.us>
2017-08-08 13:44:51 -0500

    Increasing amounts of placefile coverage.
    The zero values of the fs.Metadata struct definitely cause some rude behavior.  That perms=0 behavior caught me quite off guard, tbh.  But it's consistent and reasonable.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- fsOp/placefile_test.go
+++ fsOp/placefile_test.go
@@ -2,6 +2,7 @@ package fsOp
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -14,19 +15,41 @@ import (
 func TestPlaceFile(t *testing.T) {
 	Convey("PlaceFile suite:", t, func() {
 		testutil.WithTmpdir(func(tmpDir fs.AbsolutePath) {
-			Convey("Simple file placements", func() {
+			Convey("Simple file placements should work...", func() {
 				afs := osfs.New(tmpDir)
-				err := PlaceFile(afs, fs.Metadata{
-					Name: fs.MustRelPath("thing"),
-					Type: fs.Type_File,
-				}, bytes.NewBuffer([]byte("abc\n")), true)
-				So(err, ShouldBeNil)
+				Convey("Placing a file with read bits should work", func() {
+					fsErr := PlaceFile(afs, fs.Metadata{
+						Name:  fs.MustRelPath("thing"),
+						Type:  fs.Type_File,
+						Perms: 0644,
+					}, bytes.NewBuffer([]byte("abc\n")), true)
+					So(fsErr, ShouldBeNil)
+					bs, err := ioutil.ReadFile(tmpDir.Join(fs.MustRelPath("thing")).String())
+					So(err, ShouldBeNil)
+					So(string(bs), ShouldResemble, "abc\n")
+				})
+				Convey("Placing a file with *no* read bits should work", func() {
+					fsErr := PlaceFile(afs, fs.Metadata{
+						Name:  fs.MustRelPath("thing"),
+						Type:  fs.Type_File,
+						Perms: 0, // this is a meaningful zero!
+					}, bytes.NewBuffer([]byte("abc\n")), true)
+					So(fsErr, ShouldBeNil)
+					_, err := ioutil.ReadFile(tmpDir.Join(fs.MustRelPath("thing")).String())
+					So(err.Error(), ShouldContainSubstring, "permission denied")
+				})
+				Convey("File placements missing parent dirs should fail", func() {
+					fsErr := PlaceFile(afs, fs.Metadata{
+						Name: fs.MustRelPath("deeper/thing"),
+						Type: fs.Type_File,
+					}, bytes.NewBuffer([]byte("abc\n")), true)
+					So(fsErr.Error(), ShouldContainSubstring, "no such")
+				})
+			})
+			Convey("Simple dir placements should work", func() {
 				// TODO
 			})
-			Convey("Simple dir placements", func() {
-				// TODO
-			})
-			Convey("Symlink traversal checks", func() {
+			Convey("Placements that would traverse a symlink out of the base path should fail", func() {
 				// TODO
 			})
 		})
