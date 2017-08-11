51a954e77ea465735ffe28281ce8590dba974ed3
Eric Myhre <hash@exultant.us>
2017-08-11 15:03:48 -0500

    tar.Pack: comply with CheckPackProducesConsistentHash spec!
    This is our first spec compliance test in-action here in the reborn rio library, so it's SUPER exciting!!!
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ transmat/tar/tar_pack_test.go
@@ -0,0 +1,18 @@
+package tartrans
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"go.polydawn.net/rio/testutil"
+	"go.polydawn.net/rio/transmat/mixins/tests"
+)
+
+func TestTarPack(t *testing.T) {
+	Convey("Spec compliance: Tar pack", t,
+		testutil.Requires(testutil.RequiresCanManageOwnership, func() {
+			tests.CheckPackProducesConsistentHash(Pack)
+		}),
+	)
+}
