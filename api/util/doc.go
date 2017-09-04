f13cf79a9e8ec28aadca530922eefdc21d466869
Eric Myhre <hash@exultant.us>
2017-09-04 13:01:54 +0200

    remove old api package.
    It's all in the submodule now.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- api/util/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-	Package 'apiutil' contains utility functions and additional types for
-	further parsing of Timeless API serialized types.
-	These may be used in validation functions,
-	and are likely useful for some of the applications in the Timeless Stack
-	that implement real heavy lifting,
-	but they are not themselves serializable again.
-*/
-package apiutil
