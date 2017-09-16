695fed717881057a66ef55207646d7ed0b7fe297
Eric Myhre <hash@exultant.us>
2017-09-16 16:42:13 +0200

    start again on next generation of assembler.
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ banyan/treeUnpack.go
@@ -0,0 +1,29 @@
+package banyan
+
+import (
+	"go.polydawn.net/go-timeless-api"
+	"go.polydawn.net/rio/fs"
+)
+
+/*
+	Struct to gather the args for a single rio.Unpack func call.
+	(The context object and monitors are handled in a different band.)
+
+	Note the similar name to a structure in the go-timeless-api packages;
+	this one is not serializable, is internal, and
+	contains the literal set of warehouses already resolved,
+	as well as the path inline rather than in a map key, so we can sort slices.
+*/
+type UnpackSpec struct {
+	Path       fs.AbsolutePath
+	WareID     api.WareID
+	Filters    api.FilesetFilters
+	Warehouses []api.WarehouseAddr
+}
+
+// Cast slices to this type to sort by target path (which is effectively mountability order).
+type UnpackSpecByPath []UnpackSpec
+
+func (a UnpackSpecByPath) Len() int           { return len(a) }
+func (a UnpackSpecByPath) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a UnpackSpecByPath) Less(i, j int) bool { return a[i].Path.String() < a[j].Path.String() }
