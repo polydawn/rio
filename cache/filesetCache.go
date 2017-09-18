c11ec5ee25ae3b1b819e5d885ab81217d8f737e6
Eric Myhre <hash@exultant.us>
2017-09-18 15:24:24 +0200

    cache: shared behavior is this simple.
    It's possible we'll add branches to this logic to handle systems with other opinions about paths (deeper forms of IPFS integration come to mind, for example)... but right now, it's not necesssary.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cache/filesetCache.go
+++ cache/filesetCache.go
@@ -1,10 +1,17 @@
 package cache
 
 import (
+	"fmt"
+
 	"go.polydawn.net/go-timeless-api"
 	"go.polydawn.net/rio/fs"
+	whutil "go.polydawn.net/rio/warehouse/util"
 )
 
-type Cache interface {
-	ShelfFor(wareID api.WareID) fs.RelPath
+func ShelfFor(wareID api.WareID) fs.RelPath {
+	chunk1, chunk2, _ := whutil.ChunkifyHash(wareID)
+	return fs.MustRelPath(fmt.Sprintf("%s/committed/%s/%s/%s",
+		wareID.Type,
+		chunk1, chunk2, wareID.Hash,
+	))
 }
