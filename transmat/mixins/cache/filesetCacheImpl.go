09425048cb78d035cc1d03e767a3c4041d0afca2
Eric Myhre <hash@exultant.us>
2017-09-19 14:04:34 +0200

    cache: first pass on can-use-cache logic.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- transmat/mixins/cache/filesetCacheImpl.go
+++ transmat/mixins/cache/filesetCacheImpl.go
@@ -41,15 +41,37 @@ func (c cache) Unpack(
 	warehouses []api.WarehouseAddr,
 	monitor rio.Monitor,
 ) (api.WareID, error) {
+	// Absolute first thing: Check if we already have the ware in cache and can jump to placement ASAP.
+	//  (This must be first because we're willing to read cache even in "direct" mode, but
+	//  yet *not* willing to even initialize empty cache dirs in that mode.)
+	shelf := ShelfFor(wareID)
+	_, err := c.afs.Stat(shelf)
+	switch placementMode {
+	case rio.Placement_Direct:
+		switch err {
+		case nil: // If no error and it exists, use copy placer.
+			// TODO time to backtrack and fill in placer impls :)
+		default: // On any error at all (not just ErrNotExist), ignore the cache, and go direct:
+			return c.unpackTool(ctx, wareID, path, filt, rio.Placement_Direct, warehouses, monitor)
+		}
+	case rio.Placement_None, rio.Placement_Copy, rio.Placement_Mount:
+		switch Category(err) {
+		case nil: // If no error and it exists, use copy placer.
+			// TODO time to backtrack and fill in placer impls :)
+		case fs.ErrNotExists: // "not exists" is just a cache miss!  Proceed to fill it in.
+			// continue
+		default:
+			return api.WareID{}, err
+		}
+	default:
+		panic("unreachable")
+	}
+
 	// Initialize cache.
 	//  Ensure the cache commit root dir exists.
 	if err := fsOp.MkdirAll(c.afs, fs.MustRelPath(wareID.Type+"/fileset"), 0700); err != nil {
 		return api.WareID{}, Errorf(rio.ExitLocalCacheProblem, "cannot initialize cache dirs: %s", err)
 	}
-	// FIXME you still shouldn't be trying to do this in direct mode boyo
-
-	// Check if we already have it in cache and can return earlier.
-	// TODO
 
 	// Pick a temp path to unpack into.
 	var tmpPath fs.RelPath
