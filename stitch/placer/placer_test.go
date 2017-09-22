7d8bf0a3a07ef8b239b2ea88ac645be23440f26f
Eric Myhre <hash@exultant.us>
2017-09-22 12:43:16 +0200

    stitch/placer: assertions against result files.
    Use the filefixture helpers already established in the transmat mixins.  (I started writing the same things again in the testutil package before catching myself, yikes.)  The dirtime fixing feature is necessary for correct fixture setup.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- stitch/placer/placer_test.go
+++ stitch/placer/placer_test.go
@@ -1,8 +1,6 @@
 package placer
 
 import (
-	"bytes"
-	"io"
 	"testing"
 	"time"
 
@@ -10,8 +8,8 @@ import (
 
 	"go.polydawn.net/rio/fs"
 	"go.polydawn.net/rio/fs/osfs"
-	"go.polydawn.net/rio/fsOp"
 	"go.polydawn.net/rio/testutil"
+	. "go.polydawn.net/rio/transmat/mixins/tests"
 )
 
 func TestPlacers(t *testing.T) {
@@ -27,21 +25,27 @@ func specPlacerGood(placeFunc Placer) {
 	testutil.WithTmpdir(func(tmpDir fs.AbsolutePath) {
 		afs := osfs.New(tmpDir)
 		Convey("Placement of a dir should work, and maintain parent props", func() {
-			mustPlaceFile(afs, fs.Metadata{Name: fs.MustRelPath("srcParent"), Type: fs.Type_Dir, Perms: 0755, Mtime: time.Date(2004, 01, 15, 0, 0, 0, 0, time.UTC)}, nil)
-			mustPlaceFile(afs, fs.Metadata{Name: fs.MustRelPath("srcParent/content"), Type: fs.Type_Dir, Uid: 4000, Perms: 0755, Mtime: time.Date(2005, 01, 15, 0, 0, 0, 0, time.UTC)}, nil)
-			mustPlaceFile(afs, fs.Metadata{Name: fs.MustRelPath("srcParent/content/file"), Type: fs.Type_File, Perms: 0640, Mtime: time.Date(2006, 01, 15, 0, 0, 0, 0, time.UTC)}, bytes.NewBuffer([]byte("asdf")))
-			mustPlaceFile(afs, fs.Metadata{Name: fs.MustRelPath("dstParent"), Type: fs.Type_Dir, Perms: 0755, Mtime: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)}, nil)
+			PlaceFixture(afs, []FixtureFile{
+				{fs.Metadata{Name: fs.MustRelPath("srcParent"), Type: fs.Type_Dir, Perms: 0755, Mtime: time.Date(2004, 01, 15, 0, 0, 0, 0, time.UTC)}, nil},
+				{fs.Metadata{Name: fs.MustRelPath("srcParent/content"), Type: fs.Type_Dir, Uid: 4000, Perms: 0755, Mtime: time.Date(2005, 01, 15, 0, 0, 0, 0, time.UTC)}, nil},
+				{fs.Metadata{Name: fs.MustRelPath("srcParent/content/file"), Type: fs.Type_File, Perms: 0640, Mtime: time.Date(2006, 01, 15, 0, 0, 0, 0, time.UTC)}, []byte("asdf")},
+				{fs.Metadata{Name: fs.MustRelPath("dstParent"), Type: fs.Type_Dir, Perms: 0755, Mtime: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)}, nil},
+			})
 
 			So(placeFunc(tmpDir.Join(fs.MustRelPath("srcParent/content")), tmpDir.Join(fs.MustRelPath("dstParent/content")), true), ShouldBeNil)
+
+			// First check the content files and dirs.
+			So(shouldStat(afs, fs.MustRelPath("dstParent/content")), ShouldResemble, fs.Metadata{Name: fs.MustRelPath("dstParent/content"), Type: fs.Type_Dir, Uid: 4000, Perms: 0755, Mtime: time.Date(2005, 01, 15, 0, 0, 0, 0, time.UTC)})
+			So(shouldStat(afs, fs.MustRelPath("dstParent/content/file")), ShouldResemble, fs.Metadata{Name: fs.MustRelPath("dstParent/content/file"), Type: fs.Type_File, Perms: 0640, Mtime: time.Date(2006, 01, 15, 0, 0, 0, 0, time.UTC), Size: 4})
+			// Last (because you're most likely to screw this up) check the parent dir didn't get boinked.
+			So(shouldStat(afs, fs.MustRelPath("dstParent")), ShouldResemble, fs.Metadata{Name: fs.MustRelPath("dstParent"), Type: fs.Type_Dir, Perms: 0755, Mtime: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)})
 		})
 	})
 }
 
-func mustPlaceFile(afs fs.FS, fmeta fs.Metadata, body io.Reader) {
-	if fmeta.Type == fs.Type_File && body == nil {
-		body = &bytes.Buffer{}
-	}
-	if err := fsOp.PlaceFile(afs, fmeta, body, false); err != nil {
-		panic(err)
-	}
+func shouldStat(afs fs.FS, path fs.RelPath) fs.Metadata {
+	stat, err := afs.LStat(path)
+	So(err, ShouldBeNil)
+	stat.Mtime = stat.Mtime.UTC()
+	return *stat
 }
