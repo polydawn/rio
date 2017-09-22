c8ecce13728c240d628d22369c272927ae0713a8
Eric Myhre <hash@exultant.us>
2017-09-22 17:37:32 +0200

    cache: finish implementation.
    Tests absent because it's rather difficult to test it without forcing a dependency on an unpack tool itself... we'd almost need a dummy unpack implementation to make a sensibly hewn test coverage area.  Instead, we're going to go for more tests at a higher level.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- transmat/mixins/cache/filesetCacheImpl.go
+++ transmat/mixins/cache/filesetCacheImpl.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"os"
 
 	. "github.com/polydawn/go-errcat"
 
@@ -10,6 +11,8 @@ import (
 	cacheapi "go.polydawn.net/rio/cache"
 	"go.polydawn.net/rio/fs"
 	"go.polydawn.net/rio/fsOp"
+	"go.polydawn.net/rio/lib/guid"
+	"go.polydawn.net/rio/stitch/placer"
 )
 
 var ShelfFor = cacheapi.ShelfFor
@@ -19,7 +22,7 @@ func Lrn2Cache(cacheFs fs.FS, unpackTool rio.UnpackFunc) rio.UnpackFunc {
 }
 
 type cache struct {
-	afs        fs.FS
+	fs         fs.FS
 	unpackTool rio.UnpackFunc
 }
 
@@ -46,56 +49,99 @@ func (c cache) Unpack(
 	//  Right now we deal with this simply/stupidly: if you used filters, no cache for you.
 	resultWareID := wareID
 	if isUnpackAltering(filt) {
-		resultWareID = api.WareID{"-", "-"} // The value forces cache miss.
+		resultWareID = api.WareID{"-", "-"} // This value forces cache miss.
 	}
 
 	// First thing: Check if we already have the ware in cache and can jump to placement ASAP.
 	//  (This must be first because we're willing to read cache even in "direct" mode, but
 	//  yet *not* willing to even initialize empty cache dirs in that mode.)
-	shelf := ShelfFor(wareID)
-	_, err := c.afs.Stat(shelf)
-	switch placementMode {
-	case rio.Placement_Direct:
-		switch err {
-		case nil: // If no error and it exists, use copy placer.
-			// TODO time to backtrack and fill in placer impls :)
-		default: // On any error at all (not just ErrNotExist), ignore the cache, and go direct:
+	shelf := ShelfFor(resultWareID)
+	_, err := c.fs.Stat(shelf)
+	switch Category(err) {
+	case fs.ErrNotExists: // "not exists" is just a cache miss...
+		switch placementMode {
+		case rio.Placement_Direct: // In direct mode: be direct.  Do nothing to cache.
 			return c.unpackTool(ctx, wareID, path, filt, rio.Placement_Direct, warehouses, monitor)
+		default: // Everyone else: unpack into cache.
+			// pass
 		}
-	case rio.Placement_None, rio.Placement_Copy, rio.Placement_Mount:
-		switch Category(err) {
-		case nil: // If no error and it exists, use copy placer.
-			// TODO time to backtrack and fill in placer impls :)
-		case fs.ErrNotExists: // "not exists" is just a cache miss!  Proceed to fill it in.
-			// continue
+		// Unpack into the cache.
+		resultWareID, shelf, err = c.populate(ctx, wareID, filt, warehouses, monitor)
+		// Now that the cache has it, we can fallthrough into the "cache already had it" path.
+		fallthrough
+	case nil: // Cache has it!  Reaction varies.
+		absShelf := c.fs.BasePath().Join(shelf)
+		absDest := fs.MustAbsolutePath(path)
+		switch placementMode {
+		case rio.Placement_None: // If no placement, cache having it is victory!
+			return wareID, nil
+		case rio.Placement_Direct: // In direct mode, copy.
+			_, err := placer.CopyPlacer(absShelf, absDest, true)
+			return wareID, err
+		case rio.Placement_Copy: // In copy mode, ... well obviously copy.
+			_, err := placer.CopyPlacer(absShelf, absDest, true)
+			return wareID, err
+		case rio.Placement_Mount: // In mount mode, mount.
+			placerFn, err := placer.GetMountPlacer()
+			if err != nil {
+				return api.WareID{}, err
+			}
+			_, err = placerFn(absShelf, absDest, true)
+			return wareID, err
+		default:
+			panic("unreachable")
+		}
+	default:
+		// Unknown errors reading cache are mostly considered game over.  Except:
+		//  Since direct mode has no responsibility to the cache, it can still go.
+		switch placementMode {
+		case rio.Placement_Direct:
+			return c.unpackTool(ctx, wareID, path, filt, rio.Placement_Direct, warehouses, monitor)
 		default:
 			return api.WareID{}, err
 		}
-	default:
-		panic("unreachable")
 	}
+}
 
+func (c cache) populate(
+	ctx context.Context,
+	wareID api.WareID,
+	filt api.FilesetFilters,
+	warehouses []api.WarehouseAddr,
+	monitor rio.Monitor,
+) (api.WareID, fs.RelPath, error) {
 	// Initialize cache.
 	//  Ensure the cache commit root dir exists.
-	if err := fsOp.MkdirAll(c.afs, fs.MustRelPath(wareID.Type+"/fileset"), 0700); err != nil {
-		return api.WareID{}, Errorf(rio.ExitLocalCacheProblem, "cannot initialize cache dirs: %s", err)
+	if err := fsOp.MkdirAll(c.fs, fs.MustRelPath(wareID.Type+"/fileset"), 0700); err != nil {
+		return api.WareID{}, fs.RelPath{}, Errorf(rio.ErrLocalCacheProblem, "cannot initialize cache dirs: %s", err)
 	}
 
 	// Pick a temp path to unpack into.
-	var tmpPath fs.RelPath
-	tmpPathStr := c.afs.BasePath().Join(tmpPath).String()
+	tmpPath := fs.MustRelPath("./.tmp.unpack." + guid.New())
+	tmpPathStr := c.fs.BasePath().Join(tmpPath).String()
+	// Defer cleanup of the temp path.
+	//  (If we're successful, we'll have moved it out of this path before return.)
+	defer os.RemoveAll(tmpPathStr)
 	// Delegate!
-	resultWareID, err = c.unpackTool(ctx, wareID, tmpPathStr, filt, rio.Placement_Direct, warehouses, monitor)
+	resultWareID, err := c.unpackTool(ctx, wareID, tmpPathStr, filt, rio.Placement_Direct, warehouses, monitor)
 	if err != nil {
-		// Cleanup the tempdir
-		// TODO
-		return resultWareID, err
+		return resultWareID, fs.RelPath{}, err
 	}
 
 	// Successful unpack: commit it to its shelf location.
-	// TODO just an mv.
-
-	// Goto placer.
-	// TODO
-	return resultWareID, nil
+	//  This may also require mkdir'ing the prefix dirs of the shelf.
+	//  In case of race: accept our fate, assume the racing party acted in good faith,
+	//  return the shelf path anyway, and our defer'd rm will act on our wasted copy.
+	shelf := ShelfFor(resultWareID)
+	c.fs.Mkdir(shelf.Dir().Dir(), 0755)
+	c.fs.Mkdir(shelf.Dir(), 0755)
+	if err := os.Rename(tmpPathStr, c.fs.BasePath().Join(shelf).String()); err != nil {
+		if _, ok := err.(*os.LinkError); ok && os.IsExist(err) {
+			// Oh, fine.  Somebody raced us to it.
+			return resultWareID, shelf, nil
+		}
+		// Any other error: sad.
+		return resultWareID, shelf, Errorf(rio.ErrLocalCacheProblem, "error commiting %q into cache: %s", resultWareID, err)
+	}
+	return resultWareID, shelf, nil
 }
