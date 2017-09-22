8dade96030cafea75b7216332edc595dbfa399bf
Eric Myhre <hash@exultant.us>
2017-09-22 16:21:13 +0200

    warehouse: define spitting for short hashes.
    I ran into a silly situation where the cache system reuses the splitting for defining "shelf"s, but then crashes on an (intentionally invalid) sentinel value.  It seems better to just define behavior which, though it can do no better than propagating clear nonsense values, does so without fault.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- warehouse/util/util.go
+++ warehouse/util/util.go
@@ -10,7 +10,16 @@ import (
 	These are the first three, second three, and remaining bytes of the string.
 	For base58 encoded values, these first two chunks used as dir prefixes are a
 	cozy density for storing many many thousands of objects:
+
+	If the hash is too short, we return a bunch of dashes.  (The hash is probably
+	invalid semantically anyway, but we're not going to error about that here.)
+	A hash of empty string will result in a return of `"---", "---", "-"` (in other
+	words, as if the hash had been padded to a min of 7 characts, all dashes).
 */
 func ChunkifyHash(wareID api.WareID) (string, string, string) {
-	return wareID.Hash[0:3], wareID.Hash[3:6], wareID.Hash[6:]
+	hash := wareID.Hash
+	if len(hash) < 7 {
+		hash = hash + "-------"[:7-len(hash)]
+	}
+	return hash[0:3], hash[3:6], hash[6:]
 }
