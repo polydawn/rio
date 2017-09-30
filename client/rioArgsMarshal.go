d334baece205cfa14b15eb5b03235b4e396fa4d6
Eric Myhre <hash@exultant.us>
2017-09-30 13:51:38 +0200

    rioexecclient: fix arg ordering.
    Paths starting with dashes shouldn't be a problem.
    
    This is asthetically rather irritating, because we had this sort of dream that rio processes in `ps` would tend to have their most relevant parameters listed first.  No longer so.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- client/rioArgsMarshal.go
+++ client/rioArgsMarshal.go
@@ -14,10 +14,8 @@ func UnpackArgs(
 	monitor rio.Monitor,
 ) ([]string, error) {
 	// Required args.
-	args := []string{
-		"unpack", path, wareID.String(),
-		"--format=json",
-	}
+	args := []string{"unpack", "--format=json"}
+
 	// Append filters if specified.
 	//  (We could just pass 'em all even when emptystr, but let's be nice to readers of 'ps'.)
 	if filters.Uid != "" {
@@ -32,17 +30,27 @@ func UnpackArgs(
 	if filters.Sticky {
 		args = append(args, "--sticky")
 	}
+
 	// Append placement mode if specified.
 	if placementMode != "" {
 		args = append(args, "--placer="+string(placementMode))
 	}
+
 	// Append warehouses.
 	//  Giving this argument repeatedly forms a list in the rio CLI.
 	for _, wh := range warehouses {
 		args = append(args, "--source="+string(wh))
 	}
+
 	// Append monitor options.
 	//  (Of which there are currently none meaningful implemented.)
+
+	// Suffix the main bits.
+	//  This is last so we can use the "--" to terminate acceptance of flags
+	//  (which is important because, well, what if someone really does want
+	//  to unpack into path "--lol"?).
+	args = append(args, "--", path, wareID.String())
+
 	// Done!
 	return args, nil
 }
